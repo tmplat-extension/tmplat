d95de4c7ea708146e21e18004e896fff637ae166
Alasdair Mercer <mercer.alasdair@gmail.com>
2011-07-12 08:12:35 +0000

    removed jQuery dependency from notification page

--- src/js/notification.js
+++ src/js/notification.js
@@ -11,18 +11,19 @@ var notification = {
      * <p>This involves inserting and configuring the UI elements as well as the
      * insertion of localized Strings based on the result of the copy request.
      * </p>
-     * @requires jQuery
      * @see clipboard.showNotification
      */
     init: function () {
-        var bg = chrome.extension.getBackgroundPage();
-        var result = (bg.clipboard.status) ? 'copy_success' : 'copy_fail';
-        var sub = chrome.i18n.getMessage(bg.clipboard.feature + '_notification');
+        var bg = chrome.extension.getBackgroundPage(),
+            div = document.getElementById('tip'),
+            result = (bg.clipboard.status) ? 'copy_success' : 'copy_fail',
+            sub = chrome.i18n.getMessage(bg.clipboard.feature + '_notification');
         /*
          * Styles the tip and inserts relevant localized String depending on the
          * result of the copy request.
          */
-        $('#tip').addClass(result).html(chrome.i18n.getMessage(result, sub));
+        div.className = result;
+        div.innerHTML = chrome.i18n.getMessage(result, sub);
         /*
          * Important: Resets the clipboard to avoid affecting copy requests.
          * If user disabled the notifications option this is still called in
