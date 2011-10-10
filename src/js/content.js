ac79794e4dc4c84025220c640cfc80c6a4d5fb76
Alasdair Mercer <mercer.alasdair@gmail.com>
2011-10-10 15:41:51 +0200

    prevents content script conflicts for #42 using versioning

--- src/js/content.js
+++ src/js/content.js
@@ -4,42 +4,45 @@
  * on the current selection.
  */
 (function () {
-    var isMac = navigator.userAgent.toLowerCase().indexOf('mac') !== -1;
-    // Only adds listeners if previous injection isn't detected
-    if (document.body.hasAttribute('template_content_injected')) {
-        return;
-    }
-    document.body.setAttribute('template_content_injected', true);
-    window.addEventListener('keyup', function (e) {
-        if ((!isMac && e.ctrlKey && e.altKey) ||
-                (isMac && e.shiftKey && e.altKey)) {
-            chrome.extension.sendRequest({
-                data: {
-                    key: String.fromCharCode(e.keyCode).toUpperCase()
-                },
-                type: 'shortcut'
-            });
+    chrome.extension.sendRequest({type: 'version'}, function (versionData) {
+        var isMac = navigator.userAgent.toLowerCase().indexOf('mac') !== -1,
+            version = versionData.version.replace(/\./g, '');
+        // Only adds listeners if previous injection isn't detected
+        if (document.body.hasAttribute('template-v' + version)) {
+            return;
         }
-    });
-    chrome.extension.onRequest.addListener(function (request, sender,
-            sendResponse) {
-        var anchors,
-            data = {
-                text: '',
-                urls: []
-            },
-            contents,
-            selection = window.getSelection();
-        data.text = selection.toString();
-        if (selection.rangeCount > 0) {
-            contents = selection.getRangeAt(0).cloneContents();
-            if (contents) {
-                anchors = contents.querySelectorAll('a[href]');
-                for (var i = 0; i < anchors.length; i++) {
-                    data.urls.push(anchors[i].href);
+        document.body.setAttribute('template-v' + version, true);
+        window.addEventListener('keyup', function (e) {
+            if ((!isMac && e.ctrlKey && e.altKey) ||
+                    (isMac && e.shiftKey && e.altKey)) {
+                chrome.extension.sendRequest({
+                    data: {
+                        key: String.fromCharCode(e.keyCode).toUpperCase()
+                    },
+                    type: 'shortcut'
+                });
+            }
+        });
+        chrome.extension.onRequest.addListener(function (request, sender,
+                sendResponse) {
+            var anchors,
+                data = {
+                    text: '',
+                    urls: []
+                },
+                contents,
+                selection = window.getSelection();
+            data.text = selection.toString();
+            if (selection.rangeCount > 0) {
+                contents = selection.getRangeAt(0).cloneContents();
+                if (contents) {
+                    anchors = contents.querySelectorAll('a[href]');
+                    for (var i = 0; i < anchors.length; i++) {
+                        data.urls.push(anchors[i].href);
+                    }
                 }
             }
-        }
-        sendResponse(data);
+            sendResponse(data);
+        });
     });
 })();
