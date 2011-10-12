827f8232bbc857c77a0e6de469469da5129fc679
Alasdair Mercer <mercer.alasdair@gmail.com>
2011-10-12 07:24:07 +0000

    added short license to each javascript file

--- src/js/content.js
+++ src/js/content.js
@@ -1,3 +1,21 @@
+/* Copyright 2011 Alasdair Mercer
+ * 
+ * This file is part of Template.
+ * 
+ * Template is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Template is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Template. If not, see http://www.gnu.org/licenses/.
+ */
+
 /*
  * Injects listener for extension keyboard shortcuts in to page context as well
  * as creating a listener for providing the background page with information
