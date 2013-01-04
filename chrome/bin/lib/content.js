0dfc9b98c76ef91aac4653cf64beb75e540b85b1
neocotic <mercer.alasdair@gmail.com>
2013-01-04 11:37:11 +0000

    massive code restructure and process change

---
+++ chrome/bin/lib/content.js
@@ -0,0 +1,197 @@
+// [Template](http://neocotic.com/template)
+// (c) 2012 Alasdair Mercer
+// Freely distributable under the MIT license.
+// For all details and documentation:
+// <http://neocotic.com/template>
+(function() {
+  var elementBackups, elements, extractAll, getLink, getMeta, hotkeys, isEditable, parentLink, paste,
+    __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
+
+  elementBackups = {};
+
+  elements = {
+    field: null,
+    link: null,
+    other: null
+  };
+
+  hotkeys = [];
+
+  extractAll = function(array, property) {
+    var element, results, _i, _len, _ref;
+    results = [];
+    for (_i = 0, _len = array.length; _i < _len; _i++) {
+      element = array[_i];
+      if (element[property] != null) {
+        if (_ref = element[property], __indexOf.call(results, _ref) < 0) {
+          results.push(element[property]);
+        }
+      }
+    }
+    return results;
+  };
+
+  getLink = function(id, url) {
+    var _ref;
+    if (elementBackups[id] == null) {
+      return;
+    }
+    if (((_ref = elementBackups[id].link) != null ? _ref.href : void 0) === url) {
+      return elementBackups[id].link;
+    } else {
+      return parentLink(elementBackups[id].other);
+    }
+  };
+
+  getMeta = function(name, csv) {
+    var content, results, value, _i, _len, _ref, _ref1, _ref2;
+    content = (_ref = document.querySelector("meta[name='" + name + "']")) != null ? (_ref1 = _ref.content) != null ? _ref1.trim() : void 0 : void 0;
+    if (csv && (content != null)) {
+      results = [];
+      _ref2 = content.split(/\s*,\s*/);
+      for (_i = 0, _len = _ref2.length; _i < _len; _i++) {
+        value = _ref2[_i];
+        if (value.length) {
+          if (__indexOf.call(results, value) < 0) {
+            results.push(value);
+          }
+        }
+      }
+      return results;
+    } else {
+      return content;
+    }
+  };
+
+  isEditable = function(node) {
+    return (node != null) && !node.disabled && !node.readOnly;
+  };
+
+  parentLink = function(node) {
+    if (node == null) {
+      return;
+    }
+    if (node.nodeName === 'A') {
+      return node;
+    } else {
+      return parentLink(node.parentNode);
+    }
+  };
+
+  paste = function(node, value) {
+    var str;
+    if (!((node != null) || value)) {
+      return;
+    }
+    str = node.value.substr(0, node.selectionStart);
+    str += value;
+    str += node.value.substr(node.selectionEnd, node.value.length);
+    return node.value = str;
+  };
+
+  chrome.extension.sendRequest({
+    type: 'info'
+  }, function(data) {
+    var isMac;
+    hotkeys = data.hotkeys;
+    isMac = navigator.userAgent.toLowerCase().indexOf('mac') !== -1;
+    if (document.body.getAttribute(data.id) === data.version) {
+      return;
+    }
+    document.body.setAttribute(data.id, data.version);
+    window.addEventListener('contextmenu', function(e) {
+      switch (e.target.nodeName) {
+        case 'A':
+          return elements.link = e.target;
+        case 'INPUT':
+        case 'TEXTAREA':
+          return elements.field = e.target;
+        default:
+          return elements.other = e.target;
+      }
+    });
+    window.addEventListener('keydown', function(e) {
+      var key, _ref;
+      if ((!isMac && e.ctrlKey && e.altKey) || (isMac && e.shiftKey && e.altKey)) {
+        key = String.fromCharCode(e.keyCode).toUpperCase();
+        if (__indexOf.call(hotkeys, key) >= 0) {
+          if ((_ref = e.target.nodeName) === 'INPUT' || _ref === 'TEXTAREA') {
+            elements.field = e.target;
+          } else {
+            elements.field = null;
+          }
+          chrome.extension.sendRequest({
+            data: {
+              key: key
+            },
+            type: 'shortcut'
+          });
+          return e.preventDefault();
+        }
+      }
+    });
+    return chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+      var container, contents, href, images, link, links, selection, src, _i, _j, _len, _len1, _ref, _ref1, _ref2;
+      if (request.hotkeys != null) {
+        hotkeys = request.hotkeys;
+        return sendResponse();
+      }
+      if (request.id == null) {
+        return sendResponse();
+      }
+      if (request.type === 'paste') {
+        if ((request.contents != null) && isEditable((_ref = elementBackups[request.id]) != null ? _ref.field : void 0)) {
+          paste(elementBackups[request.id].field, request.contents);
+        }
+        delete elementBackups[request.id];
+        return sendResponse();
+      }
+      elementBackups[request.id] = {
+        field: request.editable || request.shortcut ? elements.field : void 0,
+        link: request.link ? elements.link : void 0,
+        other: request.link ? elements.other : void 0
+      };
+      selection = window.getSelection();
+      if (!selection.isCollapsed) {
+        if (contents = selection.getRangeAt(0).cloneContents()) {
+          container = document.createElement('div');
+          container.appendChild(contents);
+          _ref1 = container.querySelectorAll('[href]');
+          for (_i = 0, _len = _ref1.length; _i < _len; _i++) {
+            href = _ref1[_i];
+            href.href = href.href;
+          }
+          _ref2 = container.querySelectorAll('[src]');
+          for (_j = 0, _len1 = _ref2.length; _j < _len1; _j++) {
+            src = _ref2[_j];
+            src.src = src.src;
+          }
+          images = extractAll(container.querySelectorAll('img[src]'), 'src');
+          links = extractAll(container.querySelectorAll('a[href]'), 'href');
+        }
+      }
+      link = getLink(request.id, request.url);
+      return sendResponse({
+        author: getMeta('author'),
+        characterSet: document.characterSet,
+        description: getMeta('description'),
+        images: extractAll(document.images, 'src'),
+        keywords: getMeta('keywords', true),
+        lastModified: document.lastModified,
+        linkHTML: link != null ? link.innerHTML : void 0,
+        linkText: link != null ? link.textContent : void 0,
+        links: extractAll(document.links, 'href'),
+        pageHeight: window.innerHeight,
+        pageWidth: window.innerWidth,
+        referrer: document.referrer,
+        scripts: extractAll(document.scripts, 'src'),
+        selectedImages: images,
+        selectedLinks: links,
+        selection: selection.toString(),
+        selectionHTML: container != null ? container.innerHTML : void 0,
+        styleSheets: extractAll(document.styleSheets, 'href')
+      });
+    });
+  });
+
+}).call(this);
---
+++ chrome/bin/lib/store.js
@@ -0,0 +1,263 @@
+// [Template](http://neocotic.com/template)
+// (c) 2012 Alasdair Mercer
+// Freely distributable under the MIT license.
+// For all details and documentation:
+// <http://neocotic.com/template>
+(function() {
+  var Store, dig, store, tryParse, tryStringify,
+    __hasProp = {}.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
+    __slice = [].slice;
+
+  dig = function(root, path, force, parseFirst) {
+    var base, basePath, object, result;
+    if (parseFirst == null) {
+      parseFirst = true;
+    }
+    result = [root];
+    if (path && path.indexOf('.') !== -1) {
+      path = path.split('.');
+      object = base = root[basePath = path.shift()];
+      if (parseFirst) {
+        object = base = tryParse(object);
+      }
+      while (object && path.length > 1) {
+        object = object[path.shift()];
+        if (!(object != null) && force) {
+          object = {};
+        }
+      }
+      result.push(base, basePath, object, path.shift());
+    } else {
+      result.push(root, path, root, path);
+    }
+    return result;
+  };
+
+  tryParse = function(value) {
+    if (value != null) {
+      return JSON.parse(value);
+    } else {
+      return value;
+    }
+  };
+
+  tryStringify = function(value) {
+    if (value != null) {
+      return JSON.stringify(value);
+    } else {
+      return value;
+    }
+  };
+
+  store = window.store = new (Store = (function(_super) {
+
+    __extends(Store, _super);
+
+    function Store() {
+      return Store.__super__.constructor.apply(this, arguments);
+    }
+
+    Store.prototype.backup = function() {
+      var data, key, value;
+      data = {};
+      for (key in localStorage) {
+        if (!__hasProp.call(localStorage, key)) continue;
+        value = localStorage[key];
+        data[key] = value;
+      }
+      return encodeURIComponent(JSON.stringify(data));
+    };
+
+    Store.prototype.clear = function() {
+      var key, _results;
+      _results = [];
+      for (key in localStorage) {
+        if (!__hasProp.call(localStorage, key)) continue;
+        _results.push(delete localStorage[key]);
+      }
+      return _results;
+    };
+
+    Store.prototype.exists = function() {
+      var key, keys, _i, _len;
+      keys = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
+      for (_i = 0, _len = keys.length; _i < _len; _i++) {
+        key = keys[_i];
+        if (!localStorage.hasOwnProperty(key)) {
+          return false;
+        }
+      }
+      return true;
+    };
+
+    Store.prototype.get = function(key) {
+      var parts, value;
+      parts = dig(localStorage, key);
+      if (parts[3]) {
+        value = parts[3][parts[4]];
+        if (parts[3] === parts[0]) {
+          value = tryParse(value);
+        }
+      }
+      return value;
+    };
+
+    Store.prototype.init = function(keys, defaultValue) {
+      var key, _ref, _results;
+      switch (typeof keys) {
+        case 'object':
+          _results = [];
+          for (key in keys) {
+            if (!__hasProp.call(keys, key)) continue;
+            defaultValue = keys[key];
+            _results.push(this.init(key, defaultValue));
+          }
+          return _results;
+          break;
+        case 'string':
+          return this.set(keys, (_ref = this.get(keys)) != null ? _ref : defaultValue);
+      }
+    };
+
+    Store.prototype.modify = function() {
+      var callback, key, keys, value, _i, _j, _len, _results;
+      keys = 2 <= arguments.length ? __slice.call(arguments, 0, _i = arguments.length - 1) : (_i = 0, []), callback = arguments[_i++];
+      _results = [];
+      for (_j = 0, _len = keys.length; _j < _len; _j++) {
+        key = keys[_j];
+        value = this.get(key);
+        if (typeof callback === "function") {
+          callback(value, key);
+        }
+        _results.push(this.set(key, value));
+      }
+      return _results;
+    };
+
+    Store.prototype.remove = function() {
+      var key, keys, value, _i, _len, _results;
+      keys = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
+      if (keys.length === 1) {
+        value = this.get(keys[0]);
+        delete localStorage[keys[0]];
+        return value;
+      }
+      _results = [];
+      for (_i = 0, _len = keys.length; _i < _len; _i++) {
+        key = keys[_i];
+        _results.push(delete localStorage[key]);
+      }
+      return _results;
+    };
+
+    Store.prototype.rename = function(oldKey, newKey, defaultValue) {
+      if (this.exists(oldKey)) {
+        this.set(newKey, this.get(oldKey));
+        return this.remove(oldKey);
+      } else {
+        return this.set(newKey, defaultValue);
+      }
+    };
+
+    Store.prototype.restore = function(str) {
+      var data, key, value, _results;
+      data = JSON.parse(decodeURIComponent(str));
+      _results = [];
+      for (key in data) {
+        if (!__hasProp.call(data, key)) continue;
+        value = data[key];
+        _results.push(localStorage[key] = value);
+      }
+      return _results;
+    };
+
+    Store.prototype.search = function(regex) {
+      var key, _results;
+      _results = [];
+      for (key in localStorage) {
+        if (!__hasProp.call(localStorage, key)) continue;
+        if (regex.test(key)) {
+          _results.push(key);
+        }
+      }
+      return _results;
+    };
+
+    Store.prototype.set = function(keys, value) {
+      var key, oldValue, _results;
+      switch (typeof keys) {
+        case 'object':
+          _results = [];
+          for (key in keys) {
+            if (!__hasProp.call(keys, key)) continue;
+            value = keys[key];
+            _results.push(this.set(key, value));
+          }
+          return _results;
+          break;
+        case 'string':
+          oldValue = this.get(keys);
+          localStorage[keys] = tryStringify(value);
+          return oldValue;
+      }
+    };
+
+    return Store;
+
+  })(utils.Class));
+
+  store.Updater = (function(_super) {
+
+    __extends(Updater, _super);
+
+    function Updater(namespace) {
+      this.namespace = namespace;
+      this.isNew = !this.exists();
+    }
+
+    Updater.prototype.exists = function() {
+      return store.get("updates." + this.namespace) != null;
+    };
+
+    Updater.prototype.remove = function() {
+      var _this = this;
+      return store.modify('updates', function(updates) {
+        return delete updates[_this.namespace];
+      });
+    };
+
+    Updater.prototype.rename = function(namespace) {
+      var _this = this;
+      return store.modify('updates', function(updates) {
+        if (updates[_this.namespace] != null) {
+          updates[namespace] = updates[_this.namespace];
+        }
+        delete updates[_this.namespace];
+        return _this.namespace = namespace;
+      });
+    };
+
+    Updater.prototype.update = function(version, processor) {
+      var _this = this;
+      return store.modify('updates', function(updates) {
+        var _name, _ref;
+        if ((_ref = updates[_name = _this.namespace]) == null) {
+          updates[_name] = '';
+        }
+        if (updates[_this.namespace] < version) {
+          if (typeof processor === "function") {
+            processor();
+          }
+          return updates[_this.namespace] = version;
+        }
+      });
+    };
+
+    return Updater;
+
+  })(utils.Class);
+
+  store.init('updates', {});
+
+}).call(this);
